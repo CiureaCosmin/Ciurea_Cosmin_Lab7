782b3ce99258d4bbf900de305255fd5cba68d6d7
CiureaCosmin <75529179+CiureaCosmin@users.noreply.github.com>
2022-11-28 17:50:50 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Ciurea_Cosmin_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace Ciurea_Cosmin_Lab7;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Ai apasat {count} data";
+		else
+			CounterBtn.Text = $"Ai apasat doar de {count} ori";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
